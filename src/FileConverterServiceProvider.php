9ef876b23f7d7d2b68ac4a0bea2629dc25bd9e08
esdras.fragoso <esdras.fragoso@amlconsulting.com.br>
2026-04-13 01:34:45 -0300

    feat: adicionar ServiceProvider e suporte a publish de configuração
    - Adiciona FileConverterServiceProvider
    - Permite publish do config via vendor:publish
    - Integra auto-discovery com Laravel

---
+++ src/FileConverterServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace FragosoSoftware\FileConverter;
+
+use Illuminate\Support\ServiceProvider;
+
+class FileConverterServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/file-converter.php' => config_path('file-converter.php'),
+        ], 'file-converter-config');
+    }
+}
